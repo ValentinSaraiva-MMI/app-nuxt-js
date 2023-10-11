5cc43bad21fda3df115228aff2e088e1ebb0c8e7
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2023-10-11 11:30:02 +0200

    page folder

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
-    <!--test-->
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-index">Page Accueil</div>
+</template>
+
+<style></style>
+
+<script setup></script>
