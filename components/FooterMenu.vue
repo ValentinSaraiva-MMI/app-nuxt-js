18d87dc00293707a1910e823a31942037a81d85e
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2023-11-18 23:44:24 +0100

    footer

--- components/FooterMenu.vue
+++ components/FooterMenu.vue
@@ -1,33 +1,30 @@
 <template>
 
+<div class="footer__menu">
 
-
-
-<div>
-
-    <h4 v-if="title">
+    <h4 class="footer__menu__title" v-if="title">
         {{ title }}
     </h4>
 
     <ul>
-        <li v-for="link in links">
+        <li class="footer__menu__link" v-for="link in links">
 {{ link.txt }}
         </li>
     </ul>
 
 
 
-<img v-if="hasLogo" src="/image/logo.png" alt="logo site">
-<p v-if="description">
+<img class="footer__menu__logo" v-if="hasLogo" src="/image/logo.png" alt="logo site">
+<p class="footer__menu__description" v-if="description">
 {{ description }}
 </p>
 
-<nav v-if="Icons">
+<nav class="footer__menu__icons" v-if="Icons">
 <MyIcon v-for="icon in Icons" :name="icon"  color="secondary"  />   
 </nav>
 
 
-<Mymail v-if="mail" size="small" />
+<Mymail class="footer__menu__mail" v-if="mail" size="small" />
  
  
 
@@ -38,6 +35,42 @@
 </template>
 
 <style lang="scss">
+.footer__menu {
+
+max-width: 380px;
+
+&__mail {
+    margin-top: 30px;
+}
+
+&__title {
+    margin-bottom: 69px;
+}
+
+&__description {
+    margin: 30px 0 15px 0;
+     color: $gray;
+  
+}
+
+&__link {
+    color: $gray;
+   margin: 12px 0;
+
+}
+
+
+&__logo {
+    width: 180px;
+}
+
+&__icons {
+    display: flex;
+    gap: 18px;
+}
+
+}
+
 
 </style>
 
--- components/layout/Myfooter.vue
+++ components/layout/Myfooter.vue
@@ -1,6 +1,6 @@
 <template>
 
-<div> 
+<div class="footer"> 
 
 
 <FooterMenu v-for="menu in menus" v-bind="menu"/>
@@ -14,95 +14,14 @@
 </template>
 
 <style lang="scss" scoped>
-.topnav {
-  overflow: hidden;
-
-
-.header__liste{
-    @include h5;
-    display: flex;
-  
-    justify-content: center;
-    gap: 5%;
-    text-transform: capitalize;
-    align-items: center;
-    text-transform: none;
-
-    
+.footer {
+  display: flex;
+  width: 100vw;
+  justify-content: space-evenly;
+  background-color: #FAFAFF;
+  padding: 57px 0;
 }
 
-
-
-
-
-}
-
-
-
-.topnav a {
-  color: black;
-  text-decoration: none;
-}
-
-.topnav img {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-.topnav .search-container {
-  float: right;
-}
-
-.topnav input[type="text"] {
-  padding: 6px;
-  margin-top: 8px;
-  font-size: 17px;
-  border: none;
-}
-
-.topnav .search-container button {
-  float: right;
-  padding: 6px 10px;
-  margin-top: 8px;
-  margin-right: 16px;
-  background: #ddd;
-  font-size: 17px;
-  border: none;
-  cursor: pointer;
-}
-
-.topnav .search-container button:hover {
-  background: #ccc;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav .search-container {
-    float: none;
-  }
-  .topnav a,
-  .topnav input[type="text"],
-  .topnav .search-container button {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type="text"] {
-    border: 1px solid #ccc;
-  }
-}
 </style>
 
 <script setup>
