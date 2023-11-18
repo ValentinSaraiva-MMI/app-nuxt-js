9fb9658ba7959d9568627e029741ba5affdea21a
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2023-11-18 17:42:27 +0100

    correction layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,50 @@
 <template>
-  <main class="layout">
-    <article>
+  
+   
       <Header />
-    </article>
-    <aside class="layout__aside">
-      <slot name="aside"></slot>
-    </aside>
-    <article class="layout__article">
+ 
+   
+  
       <NuxtPage />
-    </article>
-    <article>
+ 
+ 
       <Footer />
-    </article>
-  </main>
+ 
+ 
 </template>
 
 <style lang="scss" scoped>
-.layout {
-  display: grid;
-  grid-template:
-    "header"
-    "article"
-    "aside"
-    "footer";
+// .layout {
+//   display: grid;
+//   grid-template:
+//     "header"
+//     "article"
+//     "aside"
+//     "footer";
 
-  @include medium-up {
-    grid-template:
-      "header header header"
-      "article article aside"
-      "footer footer footer";
-  }
+//   @include medium-up {
+//     grid-template:
+//       "header header header"
+//       "article article aside"
+//       "footer footer footer";
+//   }
 
-  > * {
-    padding: rem(16);
-  }
-  &__article {
-    grid-area: article;
-  }
-  &__aside {
-    grid-area: aside;
-  }
-  &__footer {
-    background: $primary-color;
-    grid-area: footer;
-  }
-  &__header {
-    background: $primary-color;
-    grid-area: header;
-  }
-}
+//   > * {
+//     padding: rem(16);
+//   }
+//   &__article {
+//     grid-area: article;
+//   }
+//   &__aside {
+//     grid-area: aside;
+//   }
+//   &__footer {
+//     background: $primary-color;
+//     grid-area: footer;
+//   }
+//   &__header {
+//     background: $primary-color;
+//     grid-area: header;
+//   }
+// }
 </style>
